ae1fa5b58d8bb12750f0bf54a444ffcab501cb51
Todd Murphy <tam1998@comcast.net>
2019-10-06 10:25:38 -0400

    fixed titles and added basic code to dashboard

--- changepassword.php
+++ changepassword.php
@@ -19,7 +19,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<title>CritiqueVR - Login</title>
+	<title>CritiqueVR - Change Password</title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 <!--===============================================================================================-->	
