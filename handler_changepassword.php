35c0d927f66b3aedeff1b833288fb0f79900ff1f
Todd Murphy <tam1998@comcast.net>
2019-10-06 02:23:33 -0400

    fixed sql account filename

--- handler_changepassword.php
+++ handler_changepassword.php
@@ -4,8 +4,7 @@
             session_start();
             error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
             ini_set('display_errors' , 1);
-            include ("sqlaccount.php") ;
-            include ("passwordgenerator.php");
+            include ("account.php") ;
             $db = mysqli_connect($hostname, $username, $password, $project);
             mysqli_select_db($db, $project); 
             $pass = $_POST['password1'];
