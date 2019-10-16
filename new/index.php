e20360722f6041cf9e09dd87e0a364cc35b0a5d7
Todd Murphy <tmurphy605@icloud.com>
2019-10-16 18:47:45 -0400

    test animation

--- new/index.php
+++ new/index.php
@@ -2,14 +2,14 @@
 <html>
     <head>
         //import
-        <link rel="stylesheet" href="css/animate.min.css">
+        <link rel="stylesheet" href="animate.min.css">
         <link rel="stylesheet" href="css/style.css">
         
         <title>
         </title>
     </head>
     <body>
-    
+    <h1 class="animated infinite bounce delay-2s">Example</h1>
     
     </body>
 
