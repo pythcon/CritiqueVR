be1e7a7c36cd87133dd7848683bf74598b1e7795
Todd Murphy <tmurphy605@icloud.com>
2019-10-16 18:45:02 -0400

    added animation library

---
+++ new/index.php
@@ -0,0 +1,16 @@
+<!Doctype HTML>
+<html>
+    <head>
+        //import
+        <link rel="stylesheet" href="css/animate.min.css">
+        <link rel="stylesheet" href="css/style.css">
+        
+        <title>
+        </title>
+    </head>
+    <body>
+    
+    
+    </body>
+
+</html>
