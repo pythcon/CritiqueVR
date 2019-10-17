9ca024fd227be5679f0dbca83e178b1465389b6d
Todd Murphy <tmurphy605@icloud.com>
2019-10-16 20:10:47 -0400

    ready for progress bar information

---
+++ new/js/progressbar.js
@@ -0,0 +1,27 @@
+var progressBar = {
+  Bar : $('#progress-bar'),
+  Reset : function(){
+    if (this.Bar){
+      this.Bar.find('li').removeClass('active'); 
+    }
+  },
+  Next: function(){
+    $('#progress-bar li:not(.active):first').addClass('active');
+  },
+  Back: function(){
+    $('#progress-bar li.active:last').removeClass('active');
+  }
+}
+
+progressBar.Reset();
+
+////
+$("#Next").on('click', function(){
+  progressBar.Next();
+})
+$("#Back").on('click', function(){
+  progressBar.Back();
+})
+$("#Reset").on('click', function(){
+  progressBar.Reset();
+})
