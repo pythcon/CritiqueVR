5038c2bbd280cae151cab4594e69bb7ed2af684c
Todd Murphy <tam1998@comcast.net>
2019-11-07 15:27:08 -0500

    major updates to create room and pin

--- new/test/js/dropdown.js
+++ new/test/js/dropdown.js
@@ -19,6 +19,21 @@ function expandChangePassword(){
 }
 
 function expandFileUpload(){
-    document.getElementById("fileUploadBtn").style.height= "225px";
-    document.getElementById("fileUploadForm").style.display= "block";
+    
+    if(document.getElementById("roomName").value !== "" && document.getElementById("roomDescription").value !== "" && document.getElementById("piece").checked) {
+        $(document).ready(function() { 
+            $("#doneBtn").fadeIn("1000");
+            $("#fileUploadBtn").fadeIn("1000");
+        });
+        document.getElementById("fileUploadBtn").style.height= "250px";
+        document.getElementById("fileUploadBtn").style.height= "250px";
+        document.getElementById("fileUploadForm").style.display= "block";
+    }else{
+        alert("Fill in the Room Name, Type and Description!");
+    }
+}
+
+function expandPin(){
+    document.getElementById("perfectBtn").style.height= "225px";
+    document.getElementById("perfectForm").style.display= "block";
 }
